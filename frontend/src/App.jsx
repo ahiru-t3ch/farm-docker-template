6e07335cd7513449191d2789e256f879d38e505e
ahiru-t3ch <174530271+ahiru-t3ch@users.noreply.github.com>
2024-11-19 09:08:05 +0100

    Connect react frontend to fastapi backend
    Returning dummy data
    
    FastApi CORS to allow react
    
    ReactJs Axios for react to call endpoints

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,19 @@
+import React, {useEffect, useState} from "react";
+import axios from 'axios';
 
 function App() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    axios.get('http://localhost:8000/') // FastAPI endpoint
+      .then(response => setData(response.data))
+      .catch(error => console.error('Error fetching data:', error));
+  }, []);
 
   return (
     <>
       <h1>Dummy App</h1>
+      {data ? <p>{data.message}</p> : <p>No data</p>}
     </>
   )
 }
