7f90d086e75b974dcb33cc59478c2271863f9622
ahiru-t3ch <174530271+ahiru-t3ch@users.noreply.github.com>
2024-11-19 20:36:31 +0100

    Move component to src

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import React, {useEffect, useState} from "react";
 import axios from 'axios';
-import CreateItemForm from "../components/CreateItemForm";
+import CreateItemForm from "./components/CreateItemForm";
 
 function App() {
   const [data, setData] = useState(null);
